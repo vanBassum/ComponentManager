3f4e067a7bfe84475366e527f5bbeb9719c35717
Bas Visscher <bas.visscher24@gmail.com>
2022-08-12 17:36:30 +0200

    Made things more generic

---
+++ ComponentManager/Server/Controllers/DefaultController.cs
@@ -0,0 +1,82 @@
+﻿using ComponentManager.Server.Data;
+using ComponentManager.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComponentManager.Server.Controllers
+{
+    public abstract class DefaultController<T> : ControllerBase where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        public DefaultController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        protected abstract void CopyTo(T dst, T src);
+
+        protected abstract int GetKey(T item);
+
+
+        public PagingInfo<T> Get(int? page, int? size, string? filter, string? sort, bool? desc)
+        {
+            page ??= 0;
+            size ??= 10;
+
+            IQueryable<T> data = _context.Set<T>();
+            if (filter != null)
+                data = data.Where(a => a.ToString().ToLower().Contains(filter.ToLower()));
+
+            if (sort != null)
+                data = data.OrderDynamic(sort, desc ?? false);
+
+            var result = PagingInfo<T>.Create(data, page.Value, size.Value);
+            return result;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] T item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] T item)
+        {
+            if (ModelState.IsValid)
+            {
+                T dbItem = await _context.FindAsync<T>(GetKey(item));
+                if (dbItem != null)
+                {
+                    CopyTo(dbItem, item);
+                    _context.Update(dbItem);
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+            }
+            return BadRequest();
+        }
+
+
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            Transistor dbItem = await _context.Transistors.FindAsync(id);
+            if (dbItem != null)
+            {
+                _context.Remove(dbItem);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return NotFound();
+        }
+
+    }
+}
--- ComponentManager/Server/Controllers/TransistorController.cs
+++ ComponentManager/Server/Controllers/TransistorController.cs
@@ -4,87 +4,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ComponentManager.Server.Controllers
 {
+
     [ApiController]
     [Route("[controller]")]
-    public class TransistorController : ControllerBase
+    public class TransistorController : DefaultController<Transistor>
     {
-        private readonly ILogger<WeatherForecastController> _logger;
-        private readonly ApplicationDbContext _context;
-
-        public TransistorController(ILogger<WeatherForecastController> logger, ApplicationDbContext context)
+        public TransistorController(ILogger<WeatherForecastController> logger, ApplicationDbContext context) : base(context)
         {
-            _logger = logger;
-            _context = context;
-
-            if (!_context.Transistors.Any())
-            {
-                _context.Transistors.AddRange(Transistor.Seed);
-                _context.SaveChanges();
-            }
 
         }
 
-        public PagingInfo<Transistor> Get(int? page, int? size, string? filter, string? sort, bool? desc)
+        protected override int GetKey(Transistor item) => item.Id;
+
+        protected override void CopyTo(Transistor dst, Transistor src)
         {
-            page ??= 0;
-            size ??= 10;
-
-            IQueryable<Transistor> data = _context.Transistors;
-            if (filter != null)
-                data = data.Where(a => a.Name.ToLower().Contains(filter.ToLower()));
-
-            if (sort != null)
-                data = data.OrderDynamic(sort, desc??false);
-            
-               
-            var result = PagingInfo<Transistor>.Create(data, page.Value, size.Value);
-            return result;
-        }
-
-
-        [HttpPost]
-        public async Task<IActionResult> Create([FromBody] Transistor item)
-        {
-            if(ModelState.IsValid)
-            {
-                _context.Add(item);
-                await _context.SaveChangesAsync();
-                return Ok();                   
-            }
-            return BadRequest();
-        }
-
-        [HttpPut]
-        public async Task<IActionResult> Put([FromBody] Transistor item)
-        {
-            if (ModelState.IsValid)
-            {
-                Transistor dbItem = await _context.Transistors.FindAsync(item.Id);
-                if (dbItem != null)
-                {
-                    dbItem.Name = item.Name;
-                    dbItem.UMax = item.UMax;
-                    _context.Update(dbItem);
-                    await _context.SaveChangesAsync();
-                    return Ok();
-                }
-            }
-            return BadRequest();
-        }
-
-
-
-        [HttpDelete]
-        public async Task<IActionResult> Delete(int? id)
-        {
-            Transistor dbItem = await _context.Transistors.FindAsync(id);
-            if (dbItem != null)
-            {
-                _context.Remove(dbItem);
-                await _context.SaveChangesAsync();
-                return Ok();
-            }
-            return NotFound();
+            dst.Name = src.Name;
+            dst.UMax = src.UMax;
         }
     }
 }
