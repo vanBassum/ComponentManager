44b8e98bd22b4b7fec9242a5e319d57652a82451
Bas Visscher <bas.visscher24@gmail.com>
2022-08-12 17:24:48 +0200

    Cleanup

--- ComponentManager/Shared/Transistor.cs
+++ ComponentManager/Shared/Transistor.cs
@@ -6,10 +6,14 @@ namespace ComponentManager.Shared
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; } = "";
+        public string Name { get; set; } = "New transistor";
         public int? UMax { get; set; }
 
 
+        public override string ToString()
+        {
+            return Name;
+        }
 
 
 
